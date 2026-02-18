f4a759dfe409861a51e1f9cd0fc3622e10fcd45f
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-02-17 21:35:56 -0700

    Download sprite editor & create new scene

--- MovementDev/Assets/Scripts/GlideLocomotion.cs
+++ MovementDev/Assets/Scripts/GlideLocomotion.cs
@@ -4,6 +4,8 @@ public class GlideLocomotion : MonoBehaviour
 {
     public Transform rigRoot;
     public float velocity = 2f; // meters per second
+    public float rotationSpeed = 100f; // degrees per second
+    public Transform trackedTransform; // camera or controller, null for thumbstick
     private void Start()
     {
         if(rigRoot == null)
@@ -16,8 +18,23 @@ public class GlideLocomotion : MonoBehaviour
         if(forward != 0f)
         {
             Vector3 moveDirection = Vector3.forward;
+            if(trackedTransform != null)
+            {
+                moveDirection = trackedTransform.forward;
+                moveDirection.y = 0f;
+            }
             moveDirection *= -forward * velocity * Time.deltaTime;
             rigRoot.Translate(moveDirection);
         }
+
+        if(trackedTransform == null)
+        {
+            float sideways = Input.GetAxis("XRI_Right_Primary2DAxis_Horizontal");
+            if(sideways != 0f)
+            {
+                float rotation = sideways * rotationSpeed * Time.deltaTime;
+                rigRoot.Rotate(0, rotation, 0);
+            }
+        }    
     }
 }
